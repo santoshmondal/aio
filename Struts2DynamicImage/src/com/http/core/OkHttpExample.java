b693a7b08444b1b95214dc437fe4fda3ae513866
santoshm <santosh.ece06@gmail.com>
2015-11-04 12:11:26 +0530

    file download using ok http

--- Struts2DynamicImage/src/com/http/core/OkHttpExample.java
+++ Struts2DynamicImage/src/com/http/core/OkHttpExample.java
@@ -1,11 +1,16 @@
 package com.http.core;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.apache.commons.io.IOUtils;
 
 import com.squareup.okhttp.FormEncodingBuilder;
 import com.squareup.okhttp.Headers;
@@ -19,13 +24,33 @@ import com.squareup.okhttp.Response;
 public class OkHttpExample {
 	private static final OkHttpClient client = new OkHttpClient();
 	public static final MediaType MEDIA_TYPE_MARKDOWN = MediaType.parse("text/x-markdown; charset=UTF-8");
+	public static final MediaType MEDIA_TYPE_JSON = MediaType.parse("application/json; charset=utf-8");
 
 	public static void main(String[] args) throws Exception {
 		// doGetRequest();
 		// doPostRequest();
 		// doPostRequestBodyAsString();
 		//doPostRequestMultipart(); 
-		postingAFile();
+		// postingAFile();
+		downloadFile();
+	}
+
+	private static void downloadFile() throws Exception {
+		String url = "http://localhost:8080/samweb/rest/getfile.do";
+		String postBody = "{\"samreq\":{\"fileid\":\"/test/IntelliJIDEA.pdf\"}}";
+
+		Request request = new Request.Builder().url(url).post(RequestBody.create(MEDIA_TYPE_JSON, postBody))
+				.build();
+
+		Response response = client.newCall(request).execute();
+		if (!response.isSuccessful())
+			throw new IOException("Unexpected code " + response);
+
+		InputStream in = response.body().byteStream();
+		OutputStream out = new FileOutputStream(new File("/home/santoshm/Desktop/abc.pdf"));
+		IOUtils.copy(in,out);
+		in.close();
+		out.close();
 	}
 
 	public static void doGetRequest() throws Exception {
