6ac89ad334adf6c247dacd4fd4eb54765b7b4353
santosh <santosh.ece06@gmail.com>
2016-05-24 13:20:40 +0530

    Around

--- springmvc/src/com/helloworld/utils/CommonAspect.java
+++ springmvc/src/com/helloworld/utils/CommonAspect.java
@@ -1,5 +1,7 @@
 package com.helloworld.utils;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 public class CommonAspect {
 	public void logRequestAdvice() {
 		System.out.println("LOG REQUEST!!");
@@ -7,6 +9,20 @@ public class CommonAspect {
 	
 	public void logRequestAdvice1(String param1) {
 		System.out.println("LOG REQUEST1!! :: " + param1);
+	}
+	
+	public void logRequestAdviceAround(ProceedingJoinPoint joinpoint,  String param1) {
+		System.out.println("Around Before  :: " + param1);
+		
+		try{
+			
+			joinpoint.proceed();
+			
+		} catch(Throwable e){
+			e.printStackTrace();
+		}
+		
+		System.out.println("Around After :: " + param1);
 	}
 	
 	public void afterMethodAdvice() {
